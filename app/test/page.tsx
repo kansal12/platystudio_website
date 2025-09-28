3d7fba889c5a5f4333784b6a8c1b493700330384
chiranjiv37 <chiranjiv_rao@platy.studio>
2025-09-28 02:42:20 -0700

    test 1

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -68,41 +68,41 @@ const VimeoPlayer: React.FC = () => {
   };
 
   return (
-    <div
-      // className="relative  mx-auto  group/player  rounded-lg border border-slate-200/20 bg-slate-100/10"
-      className="relative  mx-auto  group/player overflow-hidden rounded-lg border border-slate-200/20 bg-slate-100/10"
-      // {...handlers}
-      onMouseEnter={() => {
-        setShowControls(true);
-        setSshowPlayPauseButton(true);
-      }}
-      onMouseLeave={() => {
-        if (isPlaying) {
-          setShowControls(false);
-          setSshowPlayPauseButton(false);
-        }
-      }}
-      onMouseMove={() => setShowControls(true)}
-      // Mobile touch support
-      onTouchStart={() => {
-        setShowControls(true);
-        setSshowPlayPauseButton(true);
-      }}
-    >
-      <div className="w-full h-[80vh] max-h-fit mx-auto relative aspect-video bg-black ">
-        <iframe
-          ref={iframeRef}
-          src="https://player.vimeo.com/video/1122400201?badge=0&autopause=0&watchlater=0&pip=0&title=0&like=0"
-          // width={width}
-          // height={height}
-          frameBorder="0"
-          allow=" fullscreen; picture-in-picture"
-          allowFullScreen
-          title="Vimeo Player"
-          className="absolute top-0 left-0 w-full h-full"
-        ></iframe>
-      </div>
+    // <div
+    //   // className="relative  mx-auto  group/player  rounded-lg border border-slate-200/20 bg-slate-100/10"
+    //   className="relative  mx-auto  group/player overflow-hidden rounded-lg border border-slate-200/20 bg-slate-100/10"
+    //   // {...handlers}
+    //   onMouseEnter={() => {
+    //     setShowControls(true);
+    //     setSshowPlayPauseButton(true);
+    //   }}
+    //   onMouseLeave={() => {
+    //     if (isPlaying) {
+    //       setShowControls(false);
+    //       setSshowPlayPauseButton(false);
+    //     }
+    //   }}
+    //   onMouseMove={() => setShowControls(true)}
+    //   // Mobile touch support
+    //   onTouchStart={() => {
+    //     setShowControls(true);
+    //     setSshowPlayPauseButton(true);
+    //   }}
+    // >
+    <div className="w-full h-[80vh] max-h-fit mx-auto relative aspect-video bg-black ">
+      <iframe
+        ref={iframeRef}
+        src="https://player.vimeo.com/video/1122400201?badge=0&autopause=0&watchlater=0&pip=0&title=0&like=0"
+        // width={width}
+        // height={height}
+        frameBorder="0"
+        allow=" fullscreen; picture-in-picture"
+        allowFullScreen
+        title="Vimeo Player"
+        className="absolute top-0 left-0 w-full h-full"
+      ></iframe>
     </div>
+    // </div>
   );
 };
 
