0c91897f955c9b4b16e4a963c6482a32adcd062a
chiranjiv37 <chiranjiv_rao@platy.studio>
2026-02-26 00:56:05 -0800

    Update brand logo scales and add new logo for sparrow

--- components/brand-logos.tsx
+++ components/brand-logos.tsx
@@ -8,67 +8,69 @@ const brands = [
   {
     name: "Mesh.ai",
     logo: "/brand_logs/mesh_ai.png",
-    scale: "scale-100",
+    scale: "scale-[150%]",
     paddingTop: "pt-0",
   },
   {
     name: "Enthu.ai",
     logo: "/brand_logs/enthu_ai-Photoroom.png",
-    scale: "scale-[70%]",
+    scale: "scale-[125%]",
     paddingTop: "pt-0",
   },
   {
     name: "aim2write",
     logo: "/brand_logs/aim_2_write.png",
-    scale: "scale-[150%]",
+    scale: "scale-[135%]",
     paddingTop: "pt-0",
   },
+
   {
     name: "bigDance",
     logo: "/brand_logs/big_dance.png",
-    scale: "scale-[200%]",
+    scale: "scale-[100%]",
     paddingTop: "pt-0",
+  },
+
+  {
+    name: "sparrow",
+    logo: "/brand_logs/sparrow.png",
+    scale: "scale-[150%]",
+    paddingTop: "pt-1",
   },
   {
     name: "incuboost",
     logo: "/brand_logs/incuboost.png",
-    scale: "scale-[200%]",
+    scale: "scale-[100%]",
     paddingTop: "pt-0",
   },
   {
     name: "fh",
     logo: "/brand_logs/fh.png",
-    scale: "scale-100",
+    scale: "scale-[90%]",
     paddingTop: "pt-0",
-  },
-  {
-    name: "sparrow",
-    logo: "/brand_logs/sparrow.png",
-    scale: "scale-[200%]",
-    paddingTop: "pt-1",
   },
   {
     name: "enalytical_ai",
     logo: "/brand_logs/enalytical_ai.png",
-    scale: "scale-[80%]",
+    scale: "scale-[200%]",
     paddingTop: "pt-3",
   },
   {
     name: "ak_studio",
     logo: "/brand_logs/ak_studio.png",
-    scale: "scale-[170%]",
+    scale: "scale-[100%]",
     paddingTop: "pt-0",
   },
   {
     name: "nuvo_retail",
     logo: "/brand_logs/nuvo_retail.png",
-    scale: "scale-[90%]",
+    scale: "scale-[200%]",
     paddingTop: "pt-0",
   },
   {
     name: "WiserStack",
     logo: "/brand_logs/WiserStack.jpeg",
-    scale: "scale-100",
+    scale: "scale-[80%]",
     paddingTop: "pt-0",
   },
 ];
@@ -96,14 +98,14 @@ export function BrandLogos() {
             {brands.map((brand, index) => (
               <div
                 key={index}
-                className="flex items-center justify-center px-8 "
+                className="flex items-center justify-center px-[32px] "
               >
                 <Image
                   src={brand.logo}
                   alt={brand.name}
                   width={70}
                   height={60}
-                  className={`h-[60px] w-auto object-contain transition-all duration-300 scal  ${brand.scale} ${brand.paddingTop}`}
+                  className={`h-[60px] w-[fit] object-contain transition-all duration-300 ${brand.scale}  `}
                 />
               </div>
             ))}
